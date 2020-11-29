41ea87568debded4526a6478e2d6552df3398aac
pneumatik <45317465+pneumatik@users.noreply.github.com>
2020-11-29 23:49:43 +0300

    apples

--- apples.py
+++ apples.py
@@ -1,13 +1,14 @@
 def plural_form(number, form1, form2, form3):
-    
-    q = number-1
-    if q == 0 or q%10 == 1 :
+    q = number
+    if int(q)%100 == 11:
+        print(f"{number} {form3}")
+    elif  q == 1 or int(q)%10 == 1:
         print(f"{number} {form1}")
-    elif q > 0 and q < 4:
+    elif q > 1 and q < 5 or (int(q)%10 > 1 and int(q)%10 < 5):
         print(f"{number} {form2}")
     else:
         print(f"{number} {form3}")
-   
-q = input('введите количество яблок ')
+    
+q = input('введите количество яблок: ')
 
 plural_form(int(q), 'яблоко', 'яблока', 'яблок')
